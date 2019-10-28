07093fa1c0206318734ecdadeaa3bf84afd5dfd5
Tobias Severfelt <tobias.severfelt@hotmail.com>
2019-10-28 23:09:43 +0100

    add inheritance

--- leeds.js
+++ leeds.js
@@ -1,17 +1,44 @@
-class Supporters {
-    numberOfClicks = 0;
-    name = document.querySelector("#supporter");
+class Person {
+    constructor(name) {
+        this.name = name;
+      }
+}
+
+class SupporterFunctions{
+    addSupporterToList(supporter){
+        let supporterList = document.querySelector("#supporters ul");
+        let supporterHtml = document.createElement("li");
+        let removeSupporterHtml = document.createElement("button");
+        
+        removeSupporterHtml.onclick = this.removeSupporter;
+        removeSupporterHtml.innerText = " Ta bort ";
+        supporterHtml.innerText = supporter.name;
+        supporterHtml.appendChild(removeSupporterHtml);
+        supporterList.appendChild(supporterHtml);
+
+    };
+    removeSupporter(){
+        let supporterList = document.querySelector("#supporters ul");
+        let htmlToRemove = this.parentElement;
+        supporterList.removeChild(htmlToRemove);
+    }
+}
+
+class Supporters extends SupporterFunctions {
 
     constructor() {
+        super();
         let button = document.getElementById("join-button");
-        button.onclick = this.clickCountMembers.bind(this);
+        button.onclick = this.addSupporter.bind(this);
       }
 
-    clickCountMembers() {
-        if (++this.numberOfClicks) {
-            alert(`Congratulations ${this.name.value}, you are member number ${this.numberOfClicks}`)
-        }
+      addSupporter() {
+        let supporter = new Person(document.querySelector("#supporter").value);
+
+        this.addSupporterToList(supporter);
     }
+
+    
 }
 
 new Supporters()
